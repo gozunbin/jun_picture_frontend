e47c1905f5a5fa198ac7be41f07b3f1d7291e436
gozunbin <930083869@qq.com>
2025-01-07 10:30:20 +0800

    AI扩图功能开发

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -9,19 +9,30 @@
     <!-- 图片   -->
     <!-- 选择上传方式 -->
     <a-tabs v-model:activeKey="uploadType"
-    >>
+      >>
       <a-tab-pane key="file" tab="文件上传">
-<!--        图片上传组件-->
+        <!--        图片上传组件-->
         <PictureUpload :picture="picture" :spaceId="spaceId" :onSuccess="onSuccess" />
       </a-tab-pane>
       <a-tab-pane key="url" tab="URL 上传" force-render>
-<!--        URL 图片上传组件-->
+        <!--        URL 图片上传组件-->
         <UrlPictureUpload :picture="picture" :spaceId="spaceId" :onSuccess="onSuccess" />
       </a-tab-pane>
     </a-tabs>
     <!-- 图片编辑   -->
     <div v-if="picture" class="edit-bar">
-      <a-button :icon="h(EditOutlined)" @click="doEditPicture">编辑图片</a-button>
+      <a-space size="middle">
+        <a-button :icon="h(EditOutlined)" @click="doEditPicture">编辑图片</a-button>
+        <a-button type="primary" ghost :icon="h(FullscreenOutlined)" @click="doImagePainting">
+          AI 扩图
+        </a-button>
+      </a-space>
+      <ImageOutPainting
+        ref="imageOutPaintingRef"
+        :picture="picture"
+        :spaceId="spaceId"
+        :onSuccess="onImageOutPaintingSuccess"
+      />
       <ImageCropper
         ref="imageCropperRef"
         :imageUrl="picture?.url"
@@ -78,8 +89,8 @@
 
 <script lang="ts" setup>
 import PictureUpload from '@/components/PictureUpload.vue'
-import UrlPictureUpload from "@/components/UrlPictureUpload.vue";
-import {computed, h, onMounted, reactive, ref} from 'vue'
+import UrlPictureUpload from '@/components/UrlPictureUpload.vue'
+import { computed, h, onMounted, reactive, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import {
   editPictureUsingPost,
@@ -87,8 +98,10 @@ import {
   listPictureTagCategoryUsingGet,
 } from '@/api/pictureController'
 import { message } from 'ant-design-vue'
-import ImageCropper from "@/components/ImageCropper.vue";
-import {EditOutlined} from "@ant-design/icons-vue";
+import ImageCropper from '@/components/ImageCropper.vue'
+import { EditOutlined, FullscreenOutlined } from '@ant-design/icons-vue'
+import ImageOutPainting from "@/components/ImageOutPainting.vue";
+
 const picture = ref<API.PictureVO>()
 const pictureForm = reactive<API.PictureEditRequest>({})
 const router = useRouter()
@@ -98,7 +111,6 @@ const uploadType = ref<'file' | 'url'>('file')
 const spaceId = computed(() => {
   return route.query?.spaceId
 })
-
 
 /**
  * 图片上传成功
@@ -198,6 +210,21 @@ const onCropSuccess = (newPicture: API.PictureVO) => {
   picture.value = newPicture
 }
 
+// AI 扩图弹窗引用
+const imageOutPaintingRef = ref()
+
+// AI 扩图
+const doImagePainting = () => {
+  if (imageOutPaintingRef.value) {
+    imageOutPaintingRef.value.openModal()
+  }
+}
+
+// 编辑成功事件
+const onImageOutPaintingSuccess = (newPicture: API.PictureVO) => {
+  picture.value = newPicture
+}
+
 </script>
 
 <style scoped>
@@ -205,9 +232,9 @@ const onCropSuccess = (newPicture: API.PictureVO) => {
   max-width: 720px;
   margin: 0 auto;
 }
-#addPicturePage .edit-bar {
-   text-align: center;
-   margin: 16px 0;
- }
 
+#addPicturePage .edit-bar {
+  text-align: center;
+  margin: 16px 0;
+}
 </style>
